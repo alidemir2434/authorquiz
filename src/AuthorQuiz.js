2aecf3abdb7ea2b53228a0c14b9f8c8261173e81
alidemir <alidemir2434@gmail.com>
2020-04-29 17:49:39 +0100

    bootstrap is added

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AuthorQuiz extends Component {
+  render(){
+    return (
+      <div>AuthorQuiz</div>
+    );
+  }
+}
+
+export default AuthorQuiz;
